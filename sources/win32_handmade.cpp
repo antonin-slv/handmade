b6e45f90e9881d801d0b5cf98f80e8f28bb11b76
Antonin <antonin.sylvestre@orange.fr>
2026-01-11 22:23:21 -0500

    day 3 : window handling + direct pixel usage

--- sources/win32_handmade.cpp
+++ sources/win32_handmade.cpp
@@ -1,103 +1,172 @@
-
 #include <Windows.h>
 
-WNDPROC Wndproc;
+// TODO : Temporary (has to get mor grannular control over what is included)
+static bool running;
+static BITMAPINFO bitmapInfo;
+static void *bitMapMemory;
+static HBITMAP BitMapHandle;
+static HDC BitMapDeviceContext;
+
+static void ResizeDIBSection(int Width, int Height)
+{
+  if (BitMapHandle)
+  {
+    DeleteObject(BitMapHandle);
+  }
+  if (!BitMapDeviceContext)
+  {
+    BitMapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+  bitmapInfo.bmiHeader.biWidth = Width;
+  bitmapInfo.bmiHeader.biHeight = -Height;
+  bitmapInfo.bmiHeader.biPlanes = 1;
+  bitmapInfo.bmiHeader.biBitCount = 32;
+  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+  bitmapInfo.bmiHeader.biXPelsPerMeter = 96;
+  bitmapInfo.bmiHeader.biYPelsPerMeter = 96;
+
+  BitMapHandle = CreateDIBSection(
+      BitMapDeviceContext,
+      &bitmapInfo,
+      DIB_RGB_COLORS,
+      &bitMapMemory,
+      nullptr, 0);
+}
+
+static void MyUpdateWindow(
+    HDC WindowContext, int X, int Y,
+    int Width, int Height)
+{
+  StretchDIBits(
+      WindowContext,
+      X, Y, Width, Height,
+      0, 0, Width, Height,
+      bitMapMemory,
+      &bitmapInfo,
+      DIB_RGB_COLORS, SRCCOPY);
+}
 
 LRESULT mainWindowCallback(
-  HWND Window,
-  UINT Message,
-  WPARAM wParam,
-  LPARAM lParam
-) {
-LRESULT rslt = 0;
+    HWND Window,
+    UINT Message,
+    WPARAM wParam,
+    LPARAM lParam)
+{
+  LRESULT rslt = 0;
 
-  switch (Message) {
-    case WM_DESTROY: {
-      OutputDebugStringA("WM_DESTROY\n");
-    } break;
-    
-    case WM_CLOSE: {
-      OutputDebugStringA("WM_CLOSE\n");
-    } break;
-    
-    case WM_SIZE: {
-      OutputDebugStringA("WM_SIZE\n");
-    } break;
-    
-    case WM_ACTIVATEAPP: {
-      OutputDebugStringA("WM_ACTIVATEAPP\n");
-    } break;
+  switch (Message)
+  {
+  case WM_DESTROY:
+  {
+    running = false;
+    OutputDebugStringA("WM_DESTROY\n");
+  }
+  break;
 
-    case WM_PAINT: {
-      PAINTSTRUCT lpPaint;
-      HDC DeviceContext = BeginPaint(Window, &lpPaint);
+  case WM_CLOSE:
+  {
+    running = false;
+    OutputDebugStringA("WM_CLOSE\n");
+  }
+  break;
 
-      LONG Height = lpPaint.rcPaint.bottom - lpPaint.rcPaint.top;
-      LONG Width  = lpPaint.rcPaint.right  - lpPaint.rcPaint.left;
+  case WM_SIZE:
+  {
+    RECT clientRect;
 
-      PatBlt(
-        DeviceContext,
-        lpPaint.rcPaint.left,
-        lpPaint.rcPaint.top,
-        Width,
-        Height,
-        BLACKNESS
-      );
+    GetClientRect(Window, &clientRect);
+    int Height = clientRect.bottom - clientRect.top;
+    int Width = clientRect.right - clientRect.left;
 
-      EndPaint(Window, &lpPaint);
-    } break;
+    ResizeDIBSection(
+        LOWORD(lParam),
+        HIWORD(lParam));
+    OutputDebugStringA("WM_SIZE\n");
+  }
+  break;
 
-    default: {
-      OutputDebugStringA("default\n");
-      rslt = DefWindowProcA(Window, Message, wParam, lParam);
-    }
+  case WM_ACTIVATEAPP:
+  {
+    OutputDebugStringA("WM_ACTIVATEAPP\n");
+  }
+  break;
+
+  case WM_PAINT:
+  {
+    PAINTSTRUCT lpPaint;
+    HDC DeviceContext = BeginPaint(Window, &lpPaint);
+
+    int Height = lpPaint.rcPaint.bottom - lpPaint.rcPaint.top;
+    int Width = lpPaint.rcPaint.right - lpPaint.rcPaint.left;
+    int X = lpPaint.rcPaint.left;
+    int Y = lpPaint.rcPaint.top;
+
+    MyUpdateWindow(DeviceContext, X, Y, Width, Height);
+
+    EndPaint(Window, &lpPaint);
+  }
+  break;
+
+  default:
+  {
+    OutputDebugStringA("default\n");
+    rslt = DefWindowProcA(Window, Message, wParam, lParam);
+  }
   }
   return rslt;
 }
 
 int WINAPI WinMain(
-  HINSTANCE Instance,
-  HINSTANCE PrevInstance,
-  LPSTR     CommandLine,
-  int       nShowCmd
-) {
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int nShowCmd)
+{
   WNDCLASS windowClass = {};
 
   windowClass.style = CS_CLASSDC | CS_HREDRAW | CS_VREDRAW;
   windowClass.lpfnWndProc = mainWindowCallback;
   windowClass.hInstance = Instance;
-  //HICON     windowClass.hIcon;
-  LPCWSTR   lpszMenuName;
+  // HICON     windowClass.hIcon;
+  LPCWSTR lpszMenuName;
   windowClass.lpszClassName = "HandmadeWindowClass";
 
-  if (RegisterClass(&windowClass)) {
+  if (RegisterClass(&windowClass))
+  {
     HWND window = CreateWindowEx(
-      0,
-      windowClass.lpszClassName,
-      "Handmade",
-      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-      CW_USEDEFAULT,
-      CW_USEDEFAULT,
-      CW_USEDEFAULT,
-      CW_USEDEFAULT,
-      nullptr,
-      nullptr,
-      Instance,
-      nullptr
-    );
+        0,
+        windowClass.lpszClassName,
+        "Handmade",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        nullptr,
+        nullptr,
+        Instance,
+        nullptr);
 
-    if (window) {
+    if (window)
+    {
       MSG message;
       BOOL getMessageResult;
-
-      while ((getMessageResult = GetMessage(&message, nullptr, 0, 0)) > 0) {
+      running = true;
+      while ((getMessageResult = GetMessage(&message, nullptr, 0, 0)) > 0 && running)
+      {
         TranslateMessage(&message);
         DispatchMessage(&message);
       }
-    } else {
+    }
+    else
+    {
       OutputDebugStringA("Failed to create window.\n");
     }
-  } else {
+  }
+  else
+  {
     OutputDebugStringA("Failed to register window class.\n");
   }
 
