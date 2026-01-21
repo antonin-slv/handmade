7a61ccb96fbf829da2a56ae2f5ca0d573d1e1487
Antonin <antonin.sylvestre@orange.fr>
2026-01-20 23:13:55 -0500

    signing the .exe so that windows is happy

--- sources/build_debug.bat
+++ sources/build_debug.bat
@@ -2,6 +2,10 @@
 
 mkdir ..\build
 pushd ..\build
+
 cl ..\sources\win32_handmade.cpp /Fe:win32_handmade.exe /Zi /Od /MDd /link User32.lib Gdi32.lib Xinput.lib Ole32.lib uuid.lib /DEBUG
 :: /F1024 -> set the stack size to 1kB 
+
+signtool sign /a /s My /n "HandmadeDev" /fd SHA256 win32_handmade.exe
+
 popd
